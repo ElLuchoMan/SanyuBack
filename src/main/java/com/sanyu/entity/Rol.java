460bcf2eb25317c54117a40f2a6b743536195155
Bryan Luis <baluist@correo.udistrital.edu.co>
2021-06-03 18:57:47 -0500

    Mapeo correcto de Rol - Contratista

--- src/main/java/com/sanyu/entity/Rol.java
+++ src/main/java/com/sanyu/entity/Rol.java
@@ -1,15 +1,22 @@
 package com.sanyu.entity;
 
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "Rol")
-public class Rol{
+public class Rol {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,5 +28,9 @@ public class Rol{
 	// @ManyToOne
 	// @JoinColumn(name = "K_IDTURNO", nullable = false)
 	// private Turno turno;
+	@JsonIgnore
+	@OneToMany(mappedBy = "documento", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@Column(name = "K_DOCUMENTO", nullable = false)
+	Set<Contratista> contratista;
 
 }
