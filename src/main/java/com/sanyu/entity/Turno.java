138a16364bcbfe595c8267574909ec7d66af942b
Bryan Luis <baluist@correo.udistrital.edu.co>
2021-06-03 18:42:11 -0500

    Agregada entidad Turno

--- src/main/java/com/sanyu/entity/Turno.java
+++ src/main/java/com/sanyu/entity/Turno.java
@@ -1,5 +1,31 @@
 package com.sanyu.entity;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Turno")
 public class Turno {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "K_IDTURNO")
+	private Long idTurno;
+	@Column(name = "F_FECHAINICIO")
+	private Date fechaInicio;
+	@Column(name = "F_FECHAFIN")
+	private Date fechaFin;
+	@Column(name = "H_HORAINICIO")
+	private String horaInicio;
+	@Column(name = "O_OBSERVACION")
+	private String observacion;
+	// @ManyToOne
+	// @JoinColumn(name = "K_IDJORNADA", nullable = false)
+	// private Jornada jornada;
 
 }
