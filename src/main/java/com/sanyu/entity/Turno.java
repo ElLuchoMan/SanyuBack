be1f08961e7c6090fd4f0b0c214686adc14827eb
Bryan Luis <baluist@correo.udistrital.edu.co>
2021-06-03 19:06:32 -0500

    Agregados getters y setters de Turno

--- src/main/java/com/sanyu/entity/Turno.java
+++ src/main/java/com/sanyu/entity/Turno.java
@@ -32,9 +32,65 @@ public class Turno {
 	private String observacion;
 	@ManyToOne
 	@JoinColumn(name = "K_IDJORNADA", nullable = false)
-	private Jornada jornada;	
+	private Jornada jornada;
 	@OneToMany(mappedBy = "idRol", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
 	@Column(name = "K_IDROL", nullable = false)
 	Set<Rol> rol;
+
+	public Number getIdTurno() {
+		return idTurno;
+	}
+
+	public void setIdTurno(Number idTurno) {
+		this.idTurno = idTurno;
+	}
+
+	public Date getFechaInicio() {
+		return fechaInicio;
+	}
+
+	public void setFechaInicio(Date fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+
+	public Date getFechaFin() {
+		return fechaFin;
+	}
+
+	public void setFechaFin(Date fechaFin) {
+		this.fechaFin = fechaFin;
+	}
+
+	public String getHoraInicio() {
+		return horaInicio;
+	}
+
+	public void setHoraInicio(String horaInicio) {
+		this.horaInicio = horaInicio;
+	}
+
+	public String getObservacion() {
+		return observacion;
+	}
+
+	public void setObservacion(String observacion) {
+		this.observacion = observacion;
+	}
+
+	public Jornada getJornada() {
+		return jornada;
+	}
+
+	public void setJornada(Jornada jornada) {
+		this.jornada = jornada;
+	}
+
+	public Set<Rol> getRol() {
+		return rol;
+	}
+
+	public void setRol(Set<Rol> rol) {
+		this.rol = rol;
+	}
 
 }
