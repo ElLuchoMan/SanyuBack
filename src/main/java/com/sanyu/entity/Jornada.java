3726419614d844959e2b617a0c8f40df91a400a4
Bryan Luis <baluist@correo.udistrital.edu.co>
2021-06-03 19:03:12 -0500

    Mapeo correcto Jornada - Turno

--- src/main/java/com/sanyu/entity/Jornada.java
+++ src/main/java/com/sanyu/entity/Jornada.java
@@ -1,10 +1,15 @@
 package com.sanyu.entity;
 
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -16,21 +21,9 @@ public class Jornada {
 	private Number idJornada;
 	@Column(name = "N_NOMBREJORNADA")
 	private String nombreJornada;
+	@OneToMany(mappedBy = "idTurno", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@Column(name = "K_IDTURNO", nullable = false)
+	Set<Turno> turno;
 
-	public Number getIdJornada() {
-		return idJornada;
-	}
-
-	public void setIdJornada(Number idJornada) {
-		this.idJornada = idJornada;
-	}
-
-	public String getNombreJornada() {
-		return nombreJornada;
-	}
-
-	public void setNombreJornada(String nombreJornada) {
-		this.nombreJornada = nombreJornada;
-	}
 
 }
