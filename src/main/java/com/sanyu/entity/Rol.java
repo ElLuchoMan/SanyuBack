fd58c0a25ae76ca56b6417473829baf4f7faf093
Bryan Luis <baluist@correo.udistrital.edu.co>
2021-06-03 19:07:12 -0500

    Agregados getters y setters de Rol

--- src/main/java/com/sanyu/entity/Rol.java
+++ src/main/java/com/sanyu/entity/Rol.java
@@ -35,4 +35,36 @@ public class Rol {
 	@Column(name = "K_DOCUMENTO", nullable = false)
 	Set<Contratista> contratista;
 
+	public Number getIdRol() {
+		return idRol;
+	}
+
+	public void setIdRol(Number idRol) {
+		this.idRol = idRol;
+	}
+
+	public String getNombreRol() {
+		return nombreRol;
+	}
+
+	public void setNombreRol(String nombreRol) {
+		this.nombreRol = nombreRol;
+	}
+
+	public Turno getTurno() {
+		return turno;
+	}
+
+	public void setTurno(Turno turno) {
+		this.turno = turno;
+	}
+
+	public Set<Contratista> getContratista() {
+		return contratista;
+	}
+
+	public void setContratista(Set<Contratista> contratista) {
+		this.contratista = contratista;
+	}
+
 }
